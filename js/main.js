4c6d10890d94fdd3691afe044dc644b1392f692e
yurii <yurii.androsiuk@gmail.com>
2019-09-22 14:16:37 +0300

    удалил if else

--- js/main.js
+++ js/main.js
@@ -71,19 +71,14 @@ function renderCards(card) {
   cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
   cardElement.querySelector('.popup__text--price').textContent = card.offer.price + '₽/ночь';
 
-  var typeOffer = '';
-  if (card.offer.type === 'flat') {
-    typeOffer = 'Квартира';
-  } else if (card.offer.type === 'bungalo') {
-    typeOffer = 'Бунгало';
-  } else if (card.offer.type === 'house') {
-    typeOffer = 'Дом';
-  } else {
-    typeOffer = 'Дворец';
-  }
+  var typeOffer = {
+    'flat': 'Квартира',
+    'bungalo': 'Бунгало',
+    'house': 'Дом',
+    'palace': 'Дворец'
+  };
 
-
-  cardElement.querySelector('.popup__type').textContent = typeOffer;
+  cardElement.querySelector('.popup__type').textContent = typeOffer[card.offer.type];
   cardElement.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей';
   cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ' , выезд до ' + card.offer.checkout;
   cardElement.querySelector('.popup__features').textContent = card.offer.features;
